1decc727e6a3bdfc9e4830555fdf1c6d9578bcd7
Awgsgghs <zedd75@mail.ru>
2026-07-27 13:43:51 +0300

    reorder solution

---
+++ linkedlist/reorderlist.py
@@ -0,0 +1,26 @@
+class Solution:
+    def reorderList(self, head: Optional[ListNode]) -> None:
+        fast = head
+        slow = head
+        while fast and fast.next:
+            fast = fast.next.next
+            slow = slow.next
+        second = slow.next
+        slow.next = None
+        prev = None
+        while second:
+            temp = second.next
+            second.next = prev
+            prev = second
+            second = temp
+        second = prev
+        first = head
+        while second:
+            temp1 = first.next
+            temp2 = second.next
+            first.next = second
+            second.next = temp1
+            first = temp1
+            second = temp2
+
+
