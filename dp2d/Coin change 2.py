a9cae2f2d98dea9e70a60e6c3fdbecc25fe2cbda
Awgsgghs <zedd75@mail.ru>
2026-08-05 15:43:30 +0300

    Coin change 2 solution

---
+++ dp2d/Coin change 2.py
@@ -0,0 +1,12 @@
+class Solution:
+    def change(self, amount: int, coins: List[int]) -> int:
+        dp=[[0 for _ in range(amount+1)] for _ in range(len(coins))]
+        for i in range(len(coins)):
+            dp[i][0]=1
+        for i in range(len(coins)):
+            for j in range(1,amount+1):
+                if i>0:
+                    dp[i][j]=dp[i-1][j]
+                if j-coins[i]>=0:
+                    dp[i][j]+=dp[i][j-coins[i]]
+        return dp[len(coins)-1][amount]
